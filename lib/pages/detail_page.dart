9c89a1bd4135c293eb0f1437b9cf2f543b84b741
Sinem Şafak <safaksm14@gmail.com>
2026-04-24 13:06:20 +0300

    dinamik icerik ve favori butonu eklendi

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
-import '../data/app_data.dart';
+import '../models/fashion_item.dart';
 
-class DetailPage extends StatelessWidget {
-  const DetailPage({super.key});
+class DetailPage extends StatefulWidget {
+  final FashionItem item;
+
+  const DetailPage({
+    super.key,
+    required this.item,
+  });
+
+  @override
+  State<DetailPage> createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  bool isFavorite = false;
 
   @override
   Widget build(BuildContext context) {
-    final item = AppData.detailItem;
+    final item = widget.item;
 
     return Scaffold(
       backgroundColor: Colors.white,
@@ -20,17 +32,17 @@ class DetailPage extends StatelessWidget {
           ),
           Positioned.fill(
             child: Container(
-              color: Colors.black.withOpacity(0.03),
+              color: Colors.black.withOpacity(0.02),
             ),
           ),
           SafeArea(
             child: Column(
               children: [
-                _buildTopBar(context),
+                buildTopBar(context),
                 const Spacer(),
-                _buildFloatingLabel(),
-                const SizedBox(height: 16),
-                _buildProductCard(item),
+                buildFloatingLabel(item.title),
+                const SizedBox(height: 14),
+                buildProductCard(item),
                 const SizedBox(height: 20),
               ],
             ),
@@ -40,20 +52,25 @@ class DetailPage extends StatelessWidget {
     );
   }
 
-  Widget _buildTopBar(BuildContext context) {
+  Widget buildTopBar(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 10),
       child: Row(
         children: [
           InkWell(
             onTap: () => Navigator.pop(context),
-            borderRadius: BorderRadius.circular(24),
-            child: const Padding(
-              padding: EdgeInsets.all(6),
-              child: Icon(
+            borderRadius: BorderRadius.circular(30),
+            child: Container(
+              width: 42,
+              height: 42,
+              decoration: BoxDecoration(
+                color: Colors.white.withOpacity(0.65),
+                shape: BoxShape.circle,
+              ),
+              child: const Icon(
                 Icons.arrow_back_ios_new,
                 color: Colors.black,
-                size: 24,
+                size: 21,
               ),
             ),
           ),
@@ -62,24 +79,23 @@ class DetailPage extends StatelessWidget {
     );
   }
 
-  Widget _buildFloatingLabel() {
+  Widget buildFloatingLabel(String title) {
     return Padding(
       padding: const EdgeInsets.only(left: 22),
       child: Align(
         alignment: Alignment.centerLeft,
         child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 8),
+          padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 9),
           decoration: BoxDecoration(
-            color: Colors.grey.withOpacity(0.55),
+            color: Colors.black.withOpacity(0.35),
             borderRadius: BorderRadius.circular(8),
           ),
-          child: const Text(
-            'LAMINATED  >',
-            style: TextStyle(
+          child: Text(
+            '$title  >',
+            style: const TextStyle(
               color: Colors.white,
               fontSize: 13,
               fontWeight: FontWeight.w700,
-              letterSpacing: 0.2,
             ),
           ),
         ),
@@ -87,7 +103,7 @@ class DetailPage extends StatelessWidget {
     );
   }
 
-  Widget _buildProductCard(item) {
+  Widget buildProductCard(FashionItem item) {
     return Container(
       margin: const EdgeInsets.symmetric(horizontal: 18),
       padding: const EdgeInsets.fromLTRB(12, 12, 12, 0),
@@ -96,9 +112,9 @@ class DetailPage extends StatelessWidget {
         borderRadius: BorderRadius.circular(18),
         boxShadow: [
           BoxShadow(
-            color: Colors.black.withOpacity(0.10),
-            blurRadius: 14,
-            offset: const Offset(0, 4),
+            color: Colors.black.withOpacity(0.12),
+            blurRadius: 16,
+            offset: const Offset(0, 5),
           ),
         ],
       ),
@@ -110,11 +126,11 @@ class DetailPage extends StatelessWidget {
                 width: 82,
                 height: 92,
                 decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12),
-                  color: const Color(0xFFF4F4F4),
+                  color: const Color(0xFFF3F3F3),
+                  borderRadius: BorderRadius.circular(13),
                 ),
                 child: ClipRRect(
-                  borderRadius: BorderRadius.circular(12),
+                  borderRadius: BorderRadius.circular(13),
                   child: Image.asset(
                     item.thumbnailImage,
                     fit: BoxFit.cover,
@@ -128,9 +144,11 @@ class DetailPage extends StatelessWidget {
                   children: [
                     Text(
                       item.title,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
                       style: const TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.w700,
+                        fontSize: 22,
+                        fontWeight: FontWeight.w800,
                         color: Colors.black87,
                       ),
                     ),
@@ -143,15 +161,15 @@ class DetailPage extends StatelessWidget {
                         fontWeight: FontWeight.w500,
                       ),
                     ),
-                    const SizedBox(height: 10),
+                    const SizedBox(height: 8),
                     Text(
                       item.description,
                       maxLines: 3,
                       overflow: TextOverflow.ellipsis,
                       style: const TextStyle(
                         fontSize: 12.5,
-                        color: Color(0xFFB0B0B0),
                         height: 1.4,
+                        color: Color(0xFFA5A5A5),
                       ),
                     ),
                   ],
@@ -169,24 +187,31 @@ class DetailPage extends StatelessWidget {
                   child: Text(
                     item.price,
                     style: const TextStyle(
-                      fontSize: 28,
+                      fontSize: 27,
                       fontWeight: FontWeight.w700,
-                      color: Color(0xFF7A7A7A),
+                      color: Color(0xFF777777),
                     ),
                   ),
                 ),
-                Container(
-                  width: 48,
-                  height: 48,
-                  margin: const EdgeInsets.only(right: 4),
-                  decoration: BoxDecoration(
-                    color: const Color(0xFFA56D47),
-                    borderRadius: BorderRadius.circular(24),
-                  ),
-                  child: const Icon(
-                    Icons.arrow_forward_ios,
-                    color: Colors.white,
-                    size: 20,
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      isFavorite = !isFavorite;
+                    });
+                  },
+                  child: Container(
+                    width: 47,
+                    height: 47,
+                    margin: const EdgeInsets.only(right: 6),
+                    decoration: BoxDecoration(
+                      color: const Color(0xFFA56D47),
+                      borderRadius: BorderRadius.circular(24),
+                    ),
+                    child: Icon(
+                      isFavorite ? Icons.favorite : Icons.arrow_forward_ios,
+                      color: Colors.white,
+                      size: isFavorite ? 23 : 19,
+                    ),
                   ),
                 ),
               ],
