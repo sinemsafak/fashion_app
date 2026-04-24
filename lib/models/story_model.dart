4e4c4fdb82b676c1136e1c50a235641c816bdb9d
Sinem Şafak <safaksm14@gmail.com>
2026-04-24 13:04:35 +0300

    hikaye icin goruntu ve izlenme durumu alanlari eklendi

--- lib/models/story_model.dart
+++ lib/models/story_model.dart
@@ -2,10 +2,14 @@ class StoryModel {
   final String imagePath;
   final String brandLogoPath;
   final String brandName;
+  final String sharedImagePath;
+  bool isViewed;
 
-  const StoryModel({
+  StoryModel({
     required this.imagePath,
     required this.brandLogoPath,
     required this.brandName,
+    required this.sharedImagePath,
+    this.isViewed = false,
   });
 }
